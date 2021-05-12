0817a73c06507c0e0320f8eda4be1e9bd4552046
Linnéa Y. Andersson <78200678+linneayuan@users.noreply.github.com>
2021-05-12 11:01:43 +0200

    Add files via upload

---
+++ code/deseq2.r
@@ -0,0 +1,28 @@
+directory <- "~/Desktop/Genome analysis/htseq"
+
+sampleFiles <- c('ERR2036629.txt', 'ERR2036630.txt', 'ERR2036631.txt', 'ERR2036632.txt', 
+                 'ERR2036633.txt', 'ERR2117288.txt', 'ERR2117289.txt', 'ERR2117290.txt',
+                 'ERR2117291.txt', 'ERR2117292.txt')
+sampleCondition <- c('continuous', 'continuous', 'batch', 'batch', 'continuous',
+                     'continuous', 'continuous', 'batch', 'batch', 'continuous')
+sampleTable <- data.frame(sampleName = sampleFiles,
+                          fileName = sampleFiles,
+                          condition = sampleCondition)
+
+sampleTable$condition <- factor(sampleTable$condition)
+
+library("DESeq2")
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                       directory = directory,
+                                       design= ~ condition)
+ddsHTSeq
+
+dds <- DESeq(ddsHTSeq)
+
+res <- results(dds)
+
+summary(res)
+
+plotMA(res)
+
+
